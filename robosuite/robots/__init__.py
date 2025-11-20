3586fc4c65360cb977ee71121b7b3cabc63d5a55
aaronrhim <rhimaaron@gmail.com>
2025-11-19 17:07:49 -0800

    updated inits

--- robosuite/robots/__init__.py
+++ robosuite/robots/__init__.py
@@ -20,6 +20,7 @@ ROBOT_CLASS_MAPPING = {
     "Panda": FixedBaseRobot,
     "Sawyer": FixedBaseRobot,
     "UR5e": FixedBaseRobot,
+    "Rover2025": FixedBaseRobot,
     "SpotWithArm": LeggedRobot,
     "SpotWithArmFloating": LeggedRobot,
     "PandaOmron": WheeledRobot,
